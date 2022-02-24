e93b95731854044d0d1658013856a6f75fc2a60c
vedatkokmen <vedatkoekmen@gmail.com>
2022-02-24 20:50:27 +0300

    completed functionality

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useReducer, useCallback } from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+import { getList } from "./api/picsum";
+import { actionCreators, initialState, reducer } from "./reducers/photos";
+import PhotoGrid from "./components/PhotoGrid";
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { photos, nextPage, loading, error } = state;
+
+  const fetchPhotos = useCallback(async () => {
+    dispatch(actionCreators.loading());
+
+    try {
+      const nextPhotos = await getList(nextPage);
+      dispatch(actionCreators.success(nextPhotos, nextPage));
+    } catch (e) {
+      dispatch(actionCreators.failure());
+    }
+  }, [nextPage]);
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  // We'll show an error only if the first page fails to load
+  if (photos.length === 0) {
+    if (loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator animating={true} />
+        </View>
+      );
+    }
+
+    if (error) {
+      return (
+        <View style={styles.container}>
+          <Text>Failed to load photos!</Text>
+        </View>
+      );
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PhotoGrid numColumns={3} photos={photos} onEndReached={fetchPhotos} />
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
---
+++ components/PhotoGrid.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Dimensions, FlatList, Image } from "react-native";
+
+import { formatPhotoUri } from "../api/picsum";
+
+const PhotoGrid = ({ photos, numColumns, onEndReached }) => {
+  const { width } = Dimensions.get("window");
+
+  const size = width / numColumns;
+
+  return (
+    <FlatList
+      data={photos}
+      keyExtractor={(item) => item.id}
+      numColumns={numColumns}
+      onEndReached={onEndReached}
+      renderItem={({ item }) => (
+        <Image
+          source={{
+            width: size,
+            height: size,
+            uri: formatPhotoUri(item.id, size, size),
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export default PhotoGrid;
